d0850e4c5092752dc0f8d4dd9497d11ba9bf9ff7
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-06-05 19:43:36 +0200

    Solution

--- car/serializers.py
+++ car/serializers.py
@@ -21,7 +21,9 @@ class CarSerializer(serializers.Serializer):
         instance.manufacturer = validated_data.get(
             "manufacturer", instance.manufacturer
         )
-        instance.model = validated_data.get("model", instance.model)
+        instance.model = validated_data.get(
+            "model", instance.model
+        )
         instance.horse_powers = validated_data.get(
             "horse_powers", instance.horse_powers
         )
